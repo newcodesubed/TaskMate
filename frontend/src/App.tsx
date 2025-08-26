2c11b455391fb77afeeeb809e98359df7c7fad74
newcodesubed <subedshahxi@gmail.com>
2025-08-26 14:21:22 +0545

    feat: app added for route setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
 
 
-export default function HEllo() {
+function App() {
   return (
-    <div className='text-4xl bg-amber-300'>HEllo</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
