00d117920c5cf97f89f9a129abe6e053bf686d8a
newcodesubed <subedshahxi@gmail.com>
2025-08-26 23:47:24 +0545

    added my logo

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -46,7 +46,20 @@ const Dashboard = () => {
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <div className="flex justify-between items-center mb-6">
-        <h1 className="text-3xl font-bold">My Todo List</h1>
+        {/* <h1 className="text-3xl font-bold">My Todo List</h1> */}
+        <div className='flex items-center'>
+
+        <img
+            src="../../public/logo.webp"
+            className="mx-2"
+            width={50}
+            height={33}
+            alt="Logo"
+            loading="lazy" 
+            />
+
+            <h1>Hello, What you have for today!</h1>
+            </div>
         <button onClick={handleLogout} className="px-4 py-2 bg-red-500 text-white rounded">
           Logout
         </button>
