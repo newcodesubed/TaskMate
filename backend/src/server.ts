ea01267b7937af3f553bd1612d263a01cb3dca4b
newcodesubed <subedshahxi@gmail.com>
2025-09-01 12:00:02 +0545

    feat:added server healthcheck

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,15 @@ import app from './app';
 
 const PORT = process.env.PORT || 5000;
 
+app.use('/health-check', (req,res)=>{
+  res.send({message: 'OK'})
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+
+  setTimeout(()=>{
+    console.log('This is repeating!');
+
+  }, 1000)
 });
